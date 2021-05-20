d9e6fd7682cdace1bdd5b8dd12af396c0311a3f5
Vlad Averin <vlad@orbitum.co>
2021-05-20 12:06:14 -0600

    Set up basic theme constants

---
+++ src/theme/index.js
@@ -0,0 +1,15 @@
+export const Colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+
+  primary1: "#130248",
+  primary2: "#3A1F81",
+  primary3: "#2E2C9B",
+
+  secondary1: "#00A4FF",
+  secondary2: "#FFFFFF",
+}
+
+export const FontFamily = {
+  main: "Work Sans",
+}
