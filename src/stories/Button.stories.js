cc3bdf11a4ecfc0b6444453a0dbbc185cb8fbcfa
Vlad Averin <vlad@orbitum.co>
2021-05-20 12:05:22 -0600

    Integrate Storybook for component inspection

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,38 @@
+import Button from '../components/Button.vue';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    size: { control: { type: 'select', options: ['small', 'medium', 'large'] } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { SbButton: Button },
+  template: '<sb-button @onClick="onClick" v-bind="$props" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
