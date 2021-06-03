7e956042dfa7eae731fc188a9adb94d794c444ee
Vlad Averin <vlad@orbitum.co>
2021-06-03 14:27:07 -0600

    Set the tailwind theme up with some constants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
@@ -26,7 +29,150 @@ module.exports = {
         '4': '4px',
       },
     },
-    extend: {},
+    colors: {
+      ...defaultTheme.colors,
+      
+      'f-purple-1': '#130248',
+      'f-purple-2': '#3A1F81',
+      'f-purple-3': '#2E2C9B',
+
+      'f-blue-1': '#00A4FF',
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    fontSize: {
+      ...defaultTheme.fontSize,
+      hero: [
+        '48px',
+        {
+            letterSpacing: '-0.02em;',
+            lineHeight: '96px',
+            fontWeight: 700
+        }
+      ],
+      h1: [
+        '36px',
+        {
+            letterSpacing: '-0.02em;',
+            lineHeight: '36px',
+            fontWeight: 700
+        }
+      ],
+      h2: [
+        '30px',
+        {
+            letterSpacing: '-0.01em;',
+            lineHeight: '36px',
+            fontWeight: 700
+        }
+      ],
+      h3: [
+        '28px',
+        {
+            letterSpacing: '-0.01em;',
+            lineHeight: '30px',
+            fontWeight: 700
+        }
+      ],
+      h4: [
+        '24px',
+        {
+            letterSpacing: '-0.01em;',
+            lineHeight: '28px',
+            fontWeight: 700
+        }
+      ],
+      h5: [
+        '24px',
+        {
+            letterSpacing: '-0.01em;',
+            lineHeight: '28px',
+            fontWeight: 500
+        }
+      ],
+      body: [
+        '18px',
+        {
+            letterSpacing: '-0.01em;',
+            lineHeight: '26px'
+        }
+      ],
+      caption: [
+        '16px',
+        {
+            lineHeight: '24px'
+        }
+      ],
+      caption2: [
+        '14px',
+        {
+            lineHeight: '20px'
+        }
+      ]
+    },
+    extend: {
+      backgroundImage: theme => ({
+        ...defaultTheme.backgroundImage,
+        'frabric-hero': "url('/src/assets/bg/frabric.png')",
+      }),
+      fontFamily: {
+        sans: ['Work Sans', ...defaultTheme.fontFamily.sans]
+      },
+      borderRadius: {
+        ...defaultTheme.borderRadius,
+        none: '0',
+        px: '1px',
+        sm: '0.313rem',
+        DEFAULT: '0.625rem'
+      },
+      textColor: {
+        ...defaultTheme.textColor,
+        'low-emphasis': '#575757',
+        primary: '#BFBFBF',
+        secondary: '#7F7F7F',
+        'high-emphasis': '#E3E3E3'
+      },
+      backgroundColor: {
+        ...defaultTheme.backgroundColor,
+        input: '#130248'
+      },
+      boxShadow: {
+        ...defaultTheme.boxShadow,
+        'f-purple-3-glow': '0px 57px 90px -47px RGBA(44, 48, 149, 0.15)',
+        'f-purple-3-glow-hovered': '0px 57px 90px -47px RGBA(44, 48, 149, 0.15)',
+      },
+      ringWidth: {
+        ...defaultTheme.ringWidth,
+        DEFAULT: '1px'
+      },
+      padding: {
+        ...defaultTheme.padding,
+        px: '1px',
+        '4px': '4px'
+      },
+      outline: {
+        ...defaultTheme.outline,
+        'low-emphasis': '#575757'
+      },
+      animation: {
+        ellipsis: ' ellipsis 1.25s infinite'
+      },
+      keyframes: {
+        ellipsis: {
+          '0%': { content: '"."' },
+          '33%': { content: '".."' },
+          '66%': { content: '"..."' }
+        }
+      },
+      minHeight: {
+        cardContent: '230px',
+        fitContent: 'fit-content'
+      }
+    },
   },
   variants: {
     linearBorderGradients: ['responsive', 'hover', 'dark'], // defaults to ['responsive']
