0e0568cf84b5f1439d0265257bef0a79ba8546ab
Vlad Averin <vlad@orbitum.co>
2021-06-04 16:48:27 -0600

    Update community section with data + new layout and graphics

--- src/components/features/Benefits/BenefitCardLayout.vue
+++ src/components/features/Benefits/BenefitCardLayout.vue
@@ -3,7 +3,6 @@
     <CardLayout :children="this.benefits">
       <template slot-scope="benefit">
         <BenefitCard :benefit="benefit"/>
-        <Button class="text-center" :label="benefit.cta"/>
       </template>
     </CardLayout>
   </div>
@@ -12,14 +11,12 @@
 <script>
 import CardLayout from '../../common/CardLayout'
 import BenefitCard from './BenefitCard'
-import Button from '../../common/Button'
 
 export default {
   name: 'BenefitCardLayout',
   components: {
     CardLayout,
     BenefitCard,
-    Button,
   },
   props: {
     benefits: {
