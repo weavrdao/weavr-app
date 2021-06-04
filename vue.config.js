f6e31ec41d4f1b86aea949d9baf5f32f8df0bcc7
Vlad Averin <vlad@orbitum.co>
2021-06-04 17:11:02 -0600

    Alternative loader config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        const transformAssetUrls = options.transformAssetUrls || {}
+        return {
+          ...options,
+          transformAssetUrls: {
+            video: ['src', 'poster'],
+            source: 'src',
+            img: 'src',
+            image: 'xlink:href',
+            // ..others
+            ...transformAssetUrls,
+          },
+        }
+      })
+  }
 };
