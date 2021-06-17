5e3dad73e9645600c45b2e56d702c4da925aecd5
Vlad Averin <vlad@orbitum.co>
2021-06-17 15:35:39 -0600

    Create footer section

--- src/components/layout/FooterSection.vue
+++ src/components/layout/FooterSection.vue
@@ -1,36 +1,72 @@
 <template>
-  <div class="relative flex flex-col justify-start">
-    <div class="w-full h-full pt-24" :style="footerStyle">
-      <div class="w-full max-w-screen-lg mx-auto mt-16 pt-16 px-8 lg:px-4">
-        <div class="relative flex flex-col items-center">
-          <div class="container ml-0 mr-auto max-w-3xl">
-            <div class="
-            text-white text-shadow-lg font-bold text-3xl md:text-5xl
-            ">
-              Want to know more?
+  <div>
+    <div class="relative flex flex-col justify-start">
+      <div class="w-full h-full pt-24" :style="footerStyle">
+        <div class="w-full max-w-screen-lg mx-auto mt-16 pt-16 px-8 lg:px-4">
+          <div class="
+          flex flex-col md:flex-row
+          py-24
+          ">
+            <div class="flex flex-col items-start justify-between md:w-2/4">
+              <div class="
+              text-white text-shadow-lg font-bold text-3xl md:text-5xl
+              ">
+                Want to know more?
+              </div>
+              <div class="flex flex-row">
+                <div class="h-full w-auto">
+                  <img :src="images.mailIcon" alt="Frabric Newsletter" class="h-28 md:h-38 w-auto">
+                </div>
+                <div class="flex flex-col items-start">
+                  <div class="
+                  max-w-xl font-medium text-base md:text-lg lg:text-xl
+                  mt-4 mb-4
+                  ">
+                    Sign up for our monthly newsletter!
+                  </div>
+                  <Button label="Subscribe Today" customClasses="shadow-xl mb-4" />
+                </div>
+              </div>
             </div>
-            <div class="
-            max-w-xl font-medium text-base md:text-lg lg:text-xl
-            mt-4 mb-8
-            ">
-              Sign up for our monthly newsletter!
+            <div class="flex flex-col items-center justify-between md:w-1/4">
+              <img :src="images.whitepaperIcon" alt="Frabric Whitepaper" class="h-28 md:h-46 w-auto">
+              <Button label="View Whitepaper" customClasses="shadow-xl mt-4 mb-4" />
+            </div>
+            <div class="flex flex-col items-center justify-between md:w-1/4">
+              <img :src="images.discordIcon" alt="Frabric Discord" class="h-28 md:h-46 w-auto">
+              <Button label="Join the Discord" customClasses="shadow-xl mt-4 mb-4" />
             </div>
           </div>
         </div>
-
-        <Button label="Find Out More" @onClick="navigateTo('explainer')" customClasses="shadow-xl" />
+        <div class="
+        flex flex-col justify-center w-full h-24 bg-f-purple-1
+        ">
+          <div class="
+          w-full text-center text-lg md:text-xl lg:text-2xl
+          ">
+            All rights reserved © Fractional Finance
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Button from '../common/Button'
+
 export default {
   name: 'FooterSection',
+  components: {
+    Button,
+  },
   data() {
     return {
       images: {
-        background: require('../../assets/pics/temp/mesh-footer.png')
+        background: require('../../assets/pics/temp/mesh-footer.png'),
+        mailIcon: require('../../assets/pics/reademail.svg'),
+        whitepaperIcon: require('../../assets/pics/document.svg'),
+        discordIcon: require('../../assets/pics/discord.svg')
       }
     }
   },
