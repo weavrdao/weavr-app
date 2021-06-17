3dbdba1f9dde301eec4eed284b160594a020db4c
Vlad Averin <vlad@orbitum.co>
2021-06-17 13:04:47 -0600

    Try using file-loader for SVG static loading

--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,50 @@
 module.exports = {
   publicPath: './',
   chainWebpack: (config) => {
-    config.module
-      .rule('svg')
-      .test(/\.svg$/)
-      .use('url-loader')
-      .loader('url-loader')
-      .tap(options => Object.assign(options, { esModule: false }))
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+
+    svgRule
+      .use('file-loader')
+        .loader('file-loader')
+        .tap(options => {
+          const newOptions = {
+            symbolId: '[name][hash]',
+            esModule: false
+          };
+
+          return { ...options, ...newOptions };
+        })
+        .end()
 
     config.module
       .rule('images')
       .use('url-loader')
-      .loader('url-loader')
-      .tap(options => Object.assign(options, { esModule: false }))
+        .loader('url-loader')
+        .tap(
+          options => Object.assign(options, { 
+            esModule: false
+          })
+        )
+        .end()
 
     config.module
       .rule('vue')
       .use('vue-loader')
-      .tap((options) => {
-        const transformAssetUrls = options.transformAssetUrls || {}
-        return {
-          ...options,
-          transformAssetUrls: {
-            video: ['src', 'poster'],
-            source: 'src',
-            img: 'src',
-            image: 'xlink:href',
-            // ..others
-            ...transformAssetUrls,
-          },
-        }
-      });
+        .tap((options) => {
+          const transformAssetUrls = options.transformAssetUrls || {}
+          return {
+            ...options,
+            transformAssetUrls: {
+              video: ['src', 'poster'],
+              source: 'src',
+              img: 'src',
+              image: 'xlink:href',
+              // ..others
+              ...transformAssetUrls,
+            },
+          }
+        })
+        .end()
   }
 };
