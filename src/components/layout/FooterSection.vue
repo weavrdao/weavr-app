4dc71c7f3a4c32ba2f4800e80157f6d510cfa955
Vlad Averin <vlad@orbitum.co>
2021-06-16 21:52:51 -0600

    Footer section WIP

--- src/components/layout/FooterSection.vue
+++ src/components/layout/FooterSection.vue
@@ -1,11 +1,48 @@
 <template>
-  <div class="relative w-full h-80">
-    Test
+  <div class="relative flex flex-col justify-start">
+    <div class="w-full h-full pt-24" :style="footerStyle">
+      <div class="w-full max-w-screen-lg mx-auto mt-16 pt-16 px-8 lg:px-4">
+        <div class="relative flex flex-col items-center">
+          <div class="container ml-0 mr-auto max-w-3xl">
+            <div class="
+            text-white text-shadow-lg font-bold text-3xl md:text-5xl
+            ">
+              Want to know more?
+            </div>
+            <div class="
+            max-w-xl font-medium text-base md:text-lg lg:text-xl
+            mt-4 mb-8
+            ">
+              Sign up for our monthly newsletter!
+            </div>
+          </div>
+        </div>
+
+        <Button label="Find Out More" @onClick="navigateTo('explainer')" customClasses="shadow-xl" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FooterSection'
+  name: 'FooterSection',
+  data() {
+    return {
+      images: {
+        background: require('../../assets/pics/temp/mesh-footer.png')
+      }
+    }
+  },
+  computed: {
+    footerStyle() {
+      return `
+      backgroundImage: linear-gradient(to top, rgba(0, 0, 0, 0.6) 50%, rgba(0, 0, 0, 0.0) 100%), url('${this.images.background}');
+      background-size: cover; 
+      background-repeat: no-repeat;
+      background-position: center bottom;
+      `
+    }
+  }
 }
 </script>
