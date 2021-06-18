8c4243c351df464eceeb6651a5b0899db5b77a3e
Vlad Averin <vlad@orbitum.co>
2021-06-17 21:48:02 -0600

    Turn the buggy plugin off

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
-const vhFix = require('postcss-100vh-fix')
 
 module.exports = {
   plugins: [
     tailwindcss,
-    vhFix,
     autoprefixer,
   ],
 };
