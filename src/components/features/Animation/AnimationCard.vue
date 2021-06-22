c8362ff50365a3f9c581a11e4dcd8cbe051a4a5c
Vlad Averin <vlad@orbitum.co>
2021-06-21 21:57:16 -0600

    Create AnimationCard view

---
+++ src/components/features/Animation/AnimationCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div :class="containerClasses">
+    <Card :children="[{ source: src }]">
+      <template slot-scope="src">
+        <div class="h-full w-full p-8">
+          <lottie-animation
+            ref="anim"
+            :animationData="require('@/assets/animations/temp/ease-animation.json')"
+            :loop="true"
+            :autoPlay="true"
+            class="w-full h-full rounded-sm"
+          />
+        </div>
+      </template>
+    </Card>
+  </div>
+</template>
+
+<script>
+import Card from '../../common/Card'
+import LottieAnimation from 'lottie-web-vue'
+
+export default {
+  name: 'VideoCard',
+  components: {
+    Card,
+    LottieAnimation,
+  },
+  props: {
+    src: {
+      type: Object,
+      required: true,
+    },
+    isBordered: {
+      type: Boolean,
+      default: true
+    }
+  },
+  computed: {
+    containerClasses() {
+      var classes = `
+      h-full w-full
+      rounded 
+      self-stretch
+      `
+
+      if (this.isBordered) {
+        classes += `
+        border-4 shadow-blue-1-glow
+        `
+      }
+
+      return classes
+    }
+  }
+}
+</script>
