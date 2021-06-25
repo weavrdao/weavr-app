67a5127dde1f7030c3b8938dd972d235a08e7879
Vlad Averin <vlad@orbitum.co>
2021-06-25 10:45:11 -0600

    Hide side nav

--- src/components/AppWrapper.vue
+++ src/components/AppWrapper.vue
@@ -2,19 +2,16 @@
   <div 
   id="app" 
   class="h-screen overflow-hidden flex items-start justify-between">
-    <SideNavigation/>
     <AppBody/>
   </div>
 </template>
 
 <script>
-import SideNavigation from "./layout/SideNavigation"
 import AppBody from "./AppBody"
 
 export default {
   name: 'AppWrapper',
   components: {
-    SideNavigation,
     AppBody
   }
 }
