858d472084ac842f5a2de18300afdeabaebae30b
Vlad Averin <vlad@orbitum.co>
2021-07-05 11:45:24 -0600

    Fix global gb

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -60,9 +60,22 @@ export default {
 
 <style scoped>
 .global-bg {
-  background-image: linear-gradient(to bottom, RGBA(22, 17, 86, 1.00) 0%, RGBA(8, 7, 30, 1.00) 50%), linear-gradient(to top, RGBA(44, 48, 149, 1.00) 0%, RGBA(8, 7, 30, 1.00) 50%);
+  background-image: linear-gradient(to bottom, RGBA(16, 5, 69, 1.00) 0%, RGBA(8, 7, 30, 1.00) 50%), linear-gradient(to top, RGBA(44, 48, 149, 1.00) 0%, RGBA(8, 7, 30, 1.00) 50%);
 }
 .footer-bg {
   background-image: linear-gradient(to top, RGBA(44, 48, 149, 0.50) 0%, RGBA(0, 0, 0, 0.00) 100%);
+  mask-image: 
+    /* linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.5), rgba(0,0,0,0)), */
+    radial-gradient(at 0% 35%, rgba(0,0,0,0.20) 5%, rgba(0,0,0,0) 15%),
+    radial-gradient(at 20% 45%, rgba(0,0,0,0.15) 5%, rgba(0,0,0,0) 15%),
+    radial-gradient(at 30% 60%, rgba(0,0,0,0.10) 5%, rgba(0,0,0,0) 15%),
+    radial-gradient(at 25% 65%, rgba(0,0,0,0.15) 5%, rgba(0,0,0,0) 15%),
+    radial-gradient(at 20% 75%, rgba(0,0,0,0.20) 5%, rgba(0,0,0,0) 15%),
+
+    radial-gradient(at 80% 25%, rgba(0,0,0,0.20) 5%, rgba(0,0,0,0) 15%),
+    radial-gradient(at 75% 35%, rgba(0,0,0,0.15) 5%, rgba(0,0,0,0) 15%),
+    radial-gradient(at 70% 40%, rgba(0,0,0,0.10) 5%, rgba(0,0,0,0) 15%),
+    radial-gradient(at 80% 55%, rgba(0,0,0,0.15) 5%, rgba(0,0,0,0) 15%),
+    radial-gradient(at 100% 65%, rgba(0,0,0,0.20) 5%, rgba(0,0,0,0) 15%)
 }
 </style>
