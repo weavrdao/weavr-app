e91e48d2b48c33acc4a5dc110195823e551b4545
Vlad Averin <vlad@orbitum.co>
2021-08-03 17:29:14 -0600

    Network utils update

--- src/utils/network.js
+++ src/utils/network.js
@@ -23,22 +23,23 @@ const postRequest = (
 
 /**
  * Gets the response for many requests with a timeout to each
- * @param {object} urlMap
- * @param {string} urlMap.key the actual URL to hit (e.g. https://resource/endpoint)
- * @param {string} urlMap.value the identifying value (e.g. https://resource)
+ * @param {object} urlMap URL map (see below)
+ * @param {string} urlMap.key The actual URL to hit
+ * @param {string} urlMap.value The identifying value
  *
  * @param {number} timeout timeout for any request to be considered bad
  * @param {function} validationCheck a check invoked for each response.
  *  If invalid, the response is filtered out.
  *  (response: any) => boolean
  */
-async function allRequests ({
+ async function allRequests ({
   urlMap,
   timeout,
   validationCheck
 }) {
   const urls = Object.keys(urlMap)
-  const requests = urls.map(async (url) => {
+
+  const requests = urls.map(async url => {
     return new Promise((resolve) => {
       axios({
         method: 'get',
@@ -47,35 +48,24 @@ async function allRequests ({
       })
         .then(response => {
           const isValid = validationCheck(response)
+
           if (isValid) {
             resolve(urlMap[url])
           } else {
             resolve(null)
           }
         })
-        .catch(() => {
+        .catch((thrown) => {
           resolve(null)
         })
     })
   })
+  
   const responses = (await Promise.all(requests)).filter(Boolean)
   return responses
 }
 
-function parseDataFromResponse (response) {
-  if (!response || !response.data) return null
-
-  let obj = response.data
-
-  if (obj == null) return null
-  if ((Array.isArray(obj) || typeof (obj) === 'string') && obj.length === 0) return null
-  if (Object.keys(obj).length === 0) return null
-
-  return obj
-}
-
 module.exports = {
   postRequest,
-  allRequests,
-  parseDataFromResponse,
+  allRequests
 }
