e0ac47169c1aaf3e7cd88d25582a65a5219c983f
Vlad Averin <vlad@orbitum.co>
2021-11-09 12:49:51 -0700

    Point the data layer to a new subgraph

--- src/data/network/graph/implementation/theGraphAPIClient.js
+++ src/data/network/graph/implementation/theGraphAPIClient.js
@@ -5,7 +5,7 @@ import {
 import { GraphQLAPIClient } from '../graphQLAPIClient'
 
 const client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/vladaverin24/frabric-subgraph-rinkeby',
+  uri: 'https://api.thegraph.com/subgraphs/name/0xnshuman/frabric-subgraph-rinkeby-v2',
   cache: new InMemoryCache()
 })
 
