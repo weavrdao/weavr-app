f5498d8e91d03349d63b2cda878c2671972f2951
Siddharth Singh <simsid66@gmail.com>
2021-12-31 23:08:44 +0530

    Fix app title

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
-# Frabric Web Intefrace
+# Frabric Web Interface
 
 ## Project setup
+
 ```
-npm install
+yarn # npm install freaks out about peer dependencies
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
     config
       .plugin('html')
       .tap(args => {
-          args[0].title = "Frabric";
-          return args;
+        args[0].title = "The Frabric";
+        return args;
       })
 
     const svgRule = config.module.rule('svg');
@@ -13,45 +13,45 @@ module.exports = {
 
     svgRule
       .use('file-loader')
-        .loader('file-loader')
-        .tap(options => {
-          const newOptions = {
-            symbolId: '[name][hash]',
-            esModule: false
-          };
+      .loader('file-loader')
+      .tap(options => {
+        const newOptions = {
+          symbolId: '[name][hash]',
+          esModule: false
+        };
 
-          return { ...options, ...newOptions };
-        })
-        .end()
+        return { ...options, ...newOptions };
+      })
+      .end()
 
     config.module
       .rule('images')
       .use('url-loader')
-        .loader('url-loader')
-        .tap(
-          options => Object.assign(options, { 
-            esModule: false
-          })
-        )
-        .end()
-        
+      .loader('url-loader')
+      .tap(
+        options => Object.assign(options, {
+          esModule: false
+        })
+      )
+      .end()
+
     config.module
       .rule('vue')
       .use('vue-loader')
-        .tap((options) => {
-          const transformAssetUrls = options.transformAssetUrls || {}
-          return {
-            ...options,
-            transformAssetUrls: {
-              video: ['src', 'poster'],
-              source: 'src',
-              img: 'src',
-              image: 'xlink:href',
-              // ..others
-              ...transformAssetUrls,
-            },
-          }
-        })
-        .end()
+      .tap((options) => {
+        const transformAssetUrls = options.transformAssetUrls || {}
+        return {
+          ...options,
+          transformAssetUrls: {
+            video: ['src', 'poster'],
+            source: 'src',
+            img: 'src',
+            image: 'xlink:href',
+            // ..others
+            ...transformAssetUrls,
+          },
+        }
+      })
+      .end()
   }
 };
