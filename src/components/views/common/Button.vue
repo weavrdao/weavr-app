0ce6b0c3a6b948283448056f91b197b3146e0311
Siddharth Singh <simsid66@gmail.com>
2022-01-13 19:54:00 +0530

    refactor Button to use Bulma

--- src/components/views/common/Button.vue
+++ src/components/views/common/Button.vue
@@ -1,74 +1,37 @@
 <template>
   <div>
-    <button 
-    type="button" 
-    @click="onClick" 
-    :class="classes"
-    >
+    <button type="button" @click="onClick" :class="classes">
       {{ label }}
     </button>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Button",
 
   props: {
     label: {
       type: String,
-      required: false
+      required: false,
     },
     isPrimary: {
       type: Boolean,
-      default: true
+      default: true,
     },
     customClasses: {
       type: String,
-      default: ""
-    }
+      default: "",
+    },
   },
 
   computed: {
     classes() {
-      var classes = `
+      return `
         button
-        relative
-        outline-none
-        focus:outline-none
-      `
-
-      classes += `
-        text-sm md:text-lg
-        py-2 md:py-3
-        px-6
-        rounded-xl
-      `
-
-      if (this.isPrimary) {
-        classes += `
-          bg-action-blue
-          text-gray-900
-          font-medium
-          hover:opacity-80
-        `
-      } else {
-        classes += `
-          border-gradient-br-blue-white-f-purple-1
-          border-transparent 
-          border-solid 
-          border-2
-          text-white
-          hover:border-f-blue-1
-        `
-      }
-
-      classes += `
-      ${this.customClasses}
-      `
-
-      return classes
+        ${this.isPrimary ? "is-primary" : ""}
+        ${this.customClasses}
+      `;
     },
   },
 
@@ -77,5 +40,5 @@ export default {
       this.$emit("onClick");
     },
   },
-}
+};
 </script>
