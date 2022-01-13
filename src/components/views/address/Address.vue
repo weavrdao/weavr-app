a812d27b0cfb3a76d100aef3726c0b8253d11d79
Siddharth Singh <simsid66@gmail.com>
2022-01-13 22:53:16 +0530

    make address medium-sized

--- src/components/views/address/Address.vue
+++ src/components/views/address/Address.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tag is-info is-large is-clickable" @click="onClick">
+  <div class="tag is-info is-medium is-clickable" @click="onClick">
     <span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
