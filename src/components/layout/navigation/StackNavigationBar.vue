824a47b8a0e3e5b2297c2f293bf4c0871fa760d3
Siddharth Singh <simsid66@gmail.com>
2022-05-27 23:38:50 +0530

    update thread page

--- src/components/layout/navigation/StackNavigationBar.vue
+++ src/components/layout/navigation/StackNavigationBar.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="mb-4">
-    Viewing <strong><slot></slot></strong> (<a
-      href="javascript:void(0)"
-      v-on:click="goBack"
-      >Back to previous page</a
-    >)
+  <div class="my-3">
+    <div class="is-flex is-align-items-center">
+      <a
+        href="javascript:void(0)"
+        :class="`mx-3 is-size-${size}`"
+        v-on:click="goBack"
+        ><strong>←</strong></a
+      >
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "StackNavigationBar",
+  props: {
+    size: {
+      type: Number,
+      default: 6,
+    },
+  },
   methods: {
     goBack() {
       this.$emit("onBack");
---
+++ src/components/utils/HeroImage.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="hero is-large my-0">
+    <div class="hero-body" :style="`background-image: url(${src});`"></div>
+  </section>
+</template>
+
+<style scoped>
+.hero-body {
+  background-position: center;
+  background-size: 100% auto;
+  width: 100%;
+}
+</style>
+
+<script>
+export default {
+  name: "HeroImage",
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
